b9dbed8738dd83bc259f7dba619a645f827496f5
ed522 <147557022+ed522@users.noreply.github.com>
2025-12-10 22:06:01 -0500

    add disabled-by-default console test interface

--- src/main/java/com/ed522/bcr2200/attendance/Launcher.java
+++ src/main/java/com/ed522/bcr2200/attendance/Launcher.java
@@ -1,9 +1,57 @@
 package com.ed522.bcr2200.attendance;
 
+import com.ed522.bcr2200.attendance.io.AttendanceEndpoint;
 import javafx.application.Application;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Arrays;
+
 public class Launcher {
+
     public static void main(String[] args) {
         Application.launch(BaseApplication.class, args);
+    }
+    public static void main1(String... args) throws IOException {
+        // test console app
+        System.out.println("Starting...");
+        AttendanceEndpoint endpoint = new AttendanceEndpoint();
+        System.out.println("Now discovering");
+        endpoint.connect(0, null);
+        System.out.println("Discovered!");
+        Thread thread = new Thread(() -> {
+            try {
+                endpoint.communicate();
+            } catch (IOException e) {
+                throw new RuntimeException("died", e);
+            }
+        });
+        thread.start();
+        System.out.println("Connected!");
+
+        // loop
+        // yes i do in fact want an infinite loop
+        //noinspection InfiniteLoopStatement
+        while (true) {
+            System.out.print("Enter a code: ");
+            byte[] data = new byte[1024];
+            int read = System.in.read(data);
+            data = Arrays.copyOf(data, read);
+
+            String in = new String(data, StandardCharsets.UTF_8).strip().trim();
+            Instant instant = Instant.now();
+            long code;
+            try {
+                code = Long.parseUnsignedLong(in);
+            } catch (NumberFormatException e) {
+                System.out.println("bad code");
+                continue;
+            }
+            // send
+            System.out.println("Sending code " + code);
+            endpoint.sendCode(new AttendanceEndpoint.VerificationCode(code, instant));
+            System.out.println("Sent code " + code);
+        }
     }
 }
